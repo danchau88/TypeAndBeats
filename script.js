3e6925f97c271c63572f2bac9851718a2e3b9083
Daniel Chau <danchau88@gmail.com>
2020-07-14 10:53:21 -0700

    made timer countdown

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const RANDOM_WORD_API_URL = "https://random-word-api.herokuapp.com/word?number=10";
 const wordDisplay = document.getElementById('game-display');
 const answerInput = document.getElementById('answer-input');
+const timeLeftDisplay = document.getElementById('time-left');
 
+
+function countDown () {
+  let timeLeft = 8;
+
+  let timer = setInterval(() => {
+    if (timeLeft <= 0) {
+      clearInterval(timer)
+    } 
+    timeLeftDisplay.innerHTML = timeLeft
+    timeLeft -= 1
+  }, 1000)
+}
+
+// Input match checker
 answerInput.addEventListener('input', () => {
   const arrWord = wordDisplay.querySelectorAll('span');
   const arrValue = answerInput.value.split('');
+  // starts countdown
+  countDown();
 
   let correct = true;
   arrWord.forEach((charSpan, index) => {
@@ -28,6 +45,7 @@ answerInput.addEventListener('input', () => {
   if (correct) renderNextWord()
 })
 
+// RandomWord API Getter
 const getRandomWord = () => {
   return fetch(RANDOM_WORD_API_URL)
     .then(response => response.json())
@@ -45,4 +63,5 @@ async function renderNextWord() {
   answerInput.value = null;
 };
 
+// Here to make sure that an initial word renders on reload
 renderNextWord();
